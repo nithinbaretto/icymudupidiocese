a3b3be5af25bc850fe5533ad116cb1a189157cff
ntihin baretto <nithin.baretto@niveussolutions.com>
2024-02-10 22:47:49 +0530

    addedd app index file

--- apps/website/icymudupidiocese/src/App.tsx
+++ apps/website/icymudupidiocese/src/App.tsx
@@ -4,11 +4,20 @@ import { ThemeProvider } from "@mui/material";
 import theme from "./providers/theme";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
+import { FirestoreProvider, useFirebaseApp } from "reactfire";
+import { getFirestore } from "firebase/firestore";
+import FirebaseAuthProvider from "./firebase_auth/firebase_auth_provider";
 
 function App() {
+  const app = useFirebaseApp();
+  const firestore = getFirestore(app);
   return (
     <ThemeProvider theme={theme}>
-      <RouterProvider router={routes} />
+      <FirebaseAuthProvider>
+        <FirestoreProvider sdk={firestore}>
+          <RouterProvider router={routes} />
+        </FirestoreProvider>
+      </FirebaseAuthProvider>
     </ThemeProvider>
   );
 }
--- apps/website/icymudupidiocese/src/index.tsx
+++ apps/website/icymudupidiocese/src/index.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { FirebaseAppProvider } from "reactfire";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const firebaseConfig = {
+  apiKey: "AIzaSyACnkD2FC6gXqpoMCLaD05aP-kxKLXMwK0",
+  authDomain: "icymudupi-da525.firebaseapp.com",
+  databaseURL: "https://icymudupi-da525-default-rtdb.firebaseio.com",
+  projectId: "icymudupi-da525",
+  storageBucket: "icymudupi-da525.appspot.com",
+  messagingSenderId: "816096023560",
+  appId: "1:816096023560:web:ce9bad6affaa20cba5adbd",
+  measurementId: "G-35TGDMY5XD",
+};
+root.render(
+  <React.StrictMode>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <App />
+    </FirebaseAppProvider>
+  </React.StrictMode>
+);
 reportWebVitals();
